bbaf095f4e2255b7fc13d4be34c692bd5ffbcdb3
Beato Bongco <beatobongco@gmail.com>
2018-08-19 16:29:34 +0800

    Initial refactor into Vue app

--- js/2_main.js
+++ js/2_main.js
@@ -15,9 +15,6 @@ const mtcnnParams = {
   */
   minFaceSize: 100
 }
-const myDB = Object.create(db)
-let modelLoaded = false
-let shouldInfer = false
 let forwardTimes = []
 
 // a temporary container for our training data
@@ -44,7 +41,7 @@ const trainState = {
   }
 }
 
-function updateTimeStats(timeInMs) {
+function updateTimeStats (timeInMs) {
   forwardTimes = [timeInMs].concat(forwardTimes).slice(0, 30)
   const avgTimeInMs = forwardTimes.reduce((total, t) => total + t) / forwardTimes.length
   $('#time').text(`${Math.round(avgTimeInMs)} ms`)
@@ -83,144 +80,144 @@ function getBestMatch(descriptorsByClass, queryDescriptor) {
     .reduce((best, curr) => best.distance < curr.distance ? best : curr)
 }
 
-async function run () {
+// async function run () {
 
-  // load models
-  await faceapi.loadMtcnnModel('models/')
-  await faceapi.loadFaceRecognitionModel('models/')
+//   // load models
+//   await faceapi.loadMtcnnModel('models/')
+//   await faceapi.loadFaceRecognitionModel('models/')
 
-  modelLoaded = true
-  $('#status').text('Models loaded! Loading data from localstorage...')
+//   modelLoaded = true
+//   $('#status').text('Models loaded! Loading data from localstorage...')
 
-  await myDB.init()
+//   await myDB.init()
 
-  // setup video feed
-  navigator.mediaDevices.getUserMedia({video: true}).
-    then(stream => {
-      videoEl.srcObject = stream
-    })
-}
+//   // setup video feed
+//   navigator.mediaDevices.getUserMedia({video: true}).
+//     then(stream => {
+//       videoEl.srcObject = stream
+//     })
+// }
 
-function doFaceDetection (detection, descriptor) {
-  $('#status').text('Detecting...')
-  const {x, y, height: boxHeight, width: boxWidth} = detection.getBox()
-  const bestMatch = getBestMatch(myDB.getEmbeddings(), descriptor)
-  let className, color
+// function doFaceDetection (detection, descriptor) {
+//   $('#status').text('Detecting...')
+//   const {x, y, height: boxHeight, width: boxWidth} = detection.getBox()
+//   const bestMatch = getBestMatch(myDB.getEmbeddings(), descriptor)
+//   let className, color
 
-  if (bestMatch && bestMatch.distance < maxFaceDist) {
-    className = `${bestMatch.className} (${bestMatch.distance})`
-    if (bestMatch.className.startsWith(unknownPrefix)) {
-      color = 'red'
-    } else {
-      color = 'green'
-    }
-  } else {
-    console.log('Tagged as unknown because dist was', bestMatch.distance, 'to best match', bestMatch.className)
-    // If class is unknown, assign it a number and
-    // save the embeddings to the database
-    className = unknownPrefix + myDB.getAutoIncrement()
-    color = 'red'
-    detectorCtx.drawImage(videoEl, x, y, boxHeight, boxWidth,
-                          0, 0, detectorCnv.width, detectorCnv.height)
+//   if (bestMatch && bestMatch.distance < maxFaceDist) {
+//     className = `${bestMatch.className} (${bestMatch.distance})`
+//     if (bestMatch.className.startsWith(unknownPrefix)) {
+//       color = 'red'
+//     } else {
+//       color = 'green'
+//     }
+//   } else {
+//     console.log('Tagged as unknown because dist was', bestMatch.distance, 'to best match', bestMatch.className)
+//     // If class is unknown, assign it a number and
+//     // save the embeddings to the database
+//     className = unknownPrefix + myDB.getAutoIncrement()
+//     color = 'red'
+//     detectorCtx.drawImage(videoEl, x, y, boxHeight, boxWidth,
+//                           0, 0, detectorCnv.width, detectorCnv.height)
 
-    myDB.addClass(className, [descriptor], detectorCnv.toDataURL())
-  }
+//     myDB.addClass(className, [descriptor], detectorCnv.toDataURL())
+//   }
 
-  faceapi.drawText(
-    canvasCtx,
-    x,
-    y + boxHeight + 3,
-    className,
-    Object.assign(faceapi.getDefaultDrawOptions(), { color: color, fontSize: 20 })
-  )
-}
+//   faceapi.drawText(
+//     canvasCtx,
+//     x,
+//     y + boxHeight + 3,
+//     className,
+//     Object.assign(faceapi.getDefaultDrawOptions(), { color: color, fontSize: 20 })
+//   )
+// }
 
-function train (detection, descriptor, numTrainImages = app.numTrainImages) {
-  let {data, className, image} = trainState.state
+// function train (detection, descriptor, numTrainImages = app.numTrainImages) {
+//   let {data, className, image} = trainState.state
 
-  $('#status').text(`Getting embeddings for class: ${className}... ${data.length} / ${numTrainImages}`)
-  trainState.appendData(descriptor)
+//   $('#status').text(`Getting embeddings for class: ${className}... ${data.length} / ${numTrainImages}`)
+//   trainState.appendData(descriptor)
 
-  const {x, y, height: boxHeight, width: boxWidth} = detection.getBox()
-  detectorCtx.drawImage(videoEl, x, y, boxHeight, boxWidth,
-                        0, 0, detectorCnv.width, detectorCnv.height)
+//   const {x, y, height: boxHeight, width: boxWidth} = detection.getBox()
+//   detectorCtx.drawImage(videoEl, x, y, boxHeight, boxWidth,
+//                         0, 0, detectorCnv.width, detectorCnv.height)
 
-  if (data.length === 1) {
-    trainState.setImage(detectorCnv.toDataURL())
-  }
+//   if (data.length === 1) {
+//     trainState.setImage(detectorCnv.toDataURL())
+//   }
 
-  if (data.length >= numTrainImages) {
-    // Save the embeddings to localstorage
-    myDB.addClass(className, data, image)
+//   if (data.length >= numTrainImages) {
+//     // Save the embeddings to localstorage
+//     myDB.addClass(className, data, image)
 
-    // cleanup by pausing the video feed
-    // and setting training data to an empty array
-    // videoEl.pause()
-    app.isTraining = false
-    trainState.emptyState()
-    $('#status').text('Done training!')
-  }
-}
+//     // cleanup by pausing the video feed
+//     // and setting training data to an empty array
+//     // videoEl.pause()
+//     app.isTraining = false
+//     trainState.emptyState()
+//     $('#status').text('Done training!')
+//   }
+// }
 
-async function forwardPass (mode, singleShot = false) {
-  if (videoEl.paused || videoEl.ended || !modelLoaded) {
-    return false
-  }
+// async function forwardPass (mode, singleShot = false) {
+//   if (videoEl.paused || videoEl.ended || !modelLoaded) {
+//     return false
+//   }
 
-  const {width, height} = faceapi.getMediaDimensions(videoEl)
+//   const {width, height} = faceapi.getMediaDimensions(videoEl)
 
-  // I've tried removing this, it seems explicit setting of
-  // canvas width and height is required to draw properly
-  canvas.width = width
-  canvas.height = height
+//   // I've tried removing this, it seems explicit setting of
+//   // canvas width and height is required to draw properly
+//   canvas.width = width
+//   canvas.height = height
 
-  const ts = Date.now()
+//   const ts = Date.now()
 
-  // We need this try catch block because of
-  // https://github.com/justadudewhohacks/face-api.js/issues/66
-  // If we dont have this block, inferencing of training will stop
-  try {
-    if (mode === 'warmup') {
-      console.log('Warming up')
-      $('#status').text('Warming up... Please wait just a little bit.')
-    }
+//   // We need this try catch block because of
+//   // https://github.com/justadudewhohacks/face-api.js/issues/66
+//   // If we dont have this block, inferencing of training will stop
+//   try {
+//     if (mode === 'warmup') {
+//       console.log('Warming up')
+//       $('#status').text('Warming up... Please wait just a little bit.')
+//     }
 
-    const fullFaceDescriptions = await faceapi.allFacesMtcnn(videoEl, mtcnnParams)
-    updateTimeStats(Date.now() - ts)
+//     const fullFaceDescriptions = await faceapi.allFacesMtcnn(videoEl, mtcnnParams)
+//     updateTimeStats(Date.now() - ts)
 
-    if (mode === 'warmup') {
-      $('#status').text('Ready to go!')
-      $('.initial-hide').show()
-      return
-    }
+//     if (mode === 'warmup') {
+//       $('#status').text('Ready to go!')
+//       $('.initial-hide').show()
+//       return
+//     }
 
-    fullFaceDescriptions.forEach(({detection, landmarks, descriptor}) => {
-      if (detection.score < minConfidence) {
-        return
-      }
+//     fullFaceDescriptions.forEach(({detection, landmarks, descriptor}) => {
+//       if (detection.score < minConfidence) {
+//         return
+//       }
 
-      faceapi.drawDetection('overlay', detection.forSize(width, height), {lineWidth: 2})
-      faceapi.drawLandmarks('overlay', landmarks.forSize(width, height), {lineWidth: 4})
+//       faceapi.drawDetection('overlay', detection.forSize(width, height), {lineWidth: 2})
+//       faceapi.drawLandmarks('overlay', landmarks.forSize(width, height), {lineWidth: 4})
 
-      if (mode === 'training') {
-        train(detection, descriptor)
-      } else if (mode === 'inference') {
-        doFaceDetection(detection, descriptor)
-        if (singleShot) {
-          videoEl.pause()
-          $('#status').text('Detection paused (single shot mode)')
-        }
-      }
-    })
-  }
-  catch (err) {
-    console.log(err)
-  }
-  if (shouldInfer) {
-    setTimeout(() => forwardPass(mode, singleShot))
-  } else {
-    canvasCtx.clearRect(0, 0, canvas.width, canvas.height)
-  }
-}
+//       if (mode === 'training') {
+//         train(detection, descriptor)
+//       } else if (mode === 'inference') {
+//         doFaceDetection(detection, descriptor)
+//         if (singleShot) {
+//           videoEl.pause()
+//           $('#status').text('Detection paused (single shot mode)')
+//         }
+//       }
+//     })
+//   }
+//   catch (err) {
+//     console.log(err)
+//   }
+//   if (shouldInfer) {
+//     setTimeout(() => forwardPass(mode, singleShot))
+//   } else {
+//     canvasCtx.clearRect(0, 0, canvas.width, canvas.height)
+//   }
+// }
 
-$(document).ready(run)
+// $(document).ready(run)
--- js/4_vue_app.js
+++ js/4_vue_app.js
@@ -7,27 +7,99 @@
     * most of the app will eventually be refactored in Vue.
     * globals should be ALL_CAPS
 */
+
+// All our constants should only be under one namespace
+const constants = {
+  // Max euclidean distance that system will allow
+  // before person is classified as unknown
+  maxFaceDist: 0.6,
+  // If score is any lower than minConfidence,
+  // system wont process this face
+  minConfidence: 0.99,
+  // Prefix given to unknown classes
+  unknownPrefix: 'Unknown #',
+  db: Object.create(db),
+  faceapi: faceapi,
+  // canvas elements and contexts for cleaner code
+  videoEl: document.querySelector('#inputVideo'),
+  canvas: document.getElementById('overlay'),
+  canvasCtx: document.getElementById('overlay').getContext('2d'),
+  detectorCnv: document.getElementById('detectorCnv'),
+  detectorCtx: document.getElementById('detectorCnv').getContext('2d'),
+  // These modes are flags that control the app's behavior,
+  // indicating when certain looping functions to terminate
+  modes: {
+    LOADING: 'LOADING',
+    IDLE: 'IDLE',
+    TRAINING: 'TRAINING',
+    DETECTING: 'DETECTING'
+  },
+  modelsPath: 'models/',
+  mtcnnParams: {
+    minFaceSize: 100
+  }
+}
+
 const app = new Vue({
   el: '#app',
   data: {
-    isTraining: false,
+    mode: constants.modes.LOADING,
     tab: 'info',
-    sharedState: myDB.state,
+    sharedState: constants.db.state,
+    forwardTimes: []
+  },
+  mounted: async function () {
+    const {faceapi, db, modes, modelsPath, videoEl} = constants
+    await faceapi.loadMtcnnModel(modelsPath)
+    await faceapi.loadFaceRecognitionModel(modelsPath)
+    await db.init()
+
+    // setup video feed
+    navigator.mediaDevices.getUserMedia({video: true})
+      .then(stream => {
+        videoEl.srcObject = stream
+      })
+
+    const handler = async function () {
+      console.log('Warming up the engines, my lord.')
+      await this.forwardPass()
+      videoEl.removeEventListener('canplay', handler)
+      this.setMode(modes.IDLE)
+    }.bind(this)
+
+    videoEl.addEventListener('canplay', handler)
+  },
+  computed: {
+    avgTimeInMs () {
+      if (this.forwardTimes.length > 0) {
+        return this.forwardTimes.reduce((total, t) => total + t) / this.forwardTimes.length
+      }
+    },
+    fps () {
+      return Math.round(1000 / this.avgTimeInMs)
+    },
+    avgInferenceTime () {
+      return Math.round(this.avgTimeInMs)
+    }
+  },
+  watch: {
+    mode (mode) {
+      console.log(`System is now ${mode}.`)
+    }
   },
   methods: {
     switchTab (tabName) {
       this.tab = tabName
     },
-    setTrainingFlag (val) {
-      this.isTraining = val
-    },
-    getTrainingFlag () {
-      return this.isTraining
+    setMode (mode) {
+      this.mode = mode
     },
     forwardPass: async function () {
+      const {videoEl, canvas, faceapi, mtcnnParams} = constants
       // Performs a forward pass on the network with the video element as input
-      if (videoEl.paused || videoEl.ended || !modelLoaded) {
-        return false
+      if (videoEl.paused || videoEl.ended) {
+        videoEl.play()
+        return
       }
 
       const {width, height} = faceapi.getMediaDimensions(videoEl)
@@ -44,13 +116,17 @@ const app = new Vue({
       // If we dont have this block, inferencing of training will stop
       try {
         const fullFaceDescriptions = await faceapi.allFacesMtcnn(videoEl, mtcnnParams)
-        updateTimeStats(Date.now() - ts)
+        this.updateTimeStats(Date.now() - ts)
         return fullFaceDescriptions
       } catch (err) {
         console.log(err)
       }
     },
-    drawDetection (detection, landmarks) {
+    drawDetection (detection, landmarks, color, className) {
+      // TODO: Since we refactored, we are able to customize color better
+      console.log('draw called')
+      const {faceapi, videoEl, canvas, canvasCtx,
+             detectorCnv, detectorCtx} = constants
       const {width, height} = faceapi.getMediaDimensions(videoEl)
 
       canvas.width = width
@@ -60,22 +136,108 @@ const app = new Vue({
       faceapi.drawLandmarks('overlay', landmarks.forSize(width, height), {lineWidth: 4})
 
       const {x, y, height: boxHeight, width: boxWidth} = detection.getBox()
-      detectorCtx.drawImage(videoEl, x, y, boxHeight, boxWidth,
-                            0, 0, detectorCnv.width, detectorCnv.height)
+      // detectorCtx.drawImage(videoEl, x, y, boxHeight, boxWidth,
+      //                       0, 0, detectorCnv.width, detectorCnv.height)
+
+      if (className) {
+        faceapi.drawText(
+          canvasCtx,
+          x,
+          y + boxHeight + 3,
+          className,
+          Object.assign(faceapi.getDefaultDrawOptions(), { color: color, fontSize: 20 })
+        )
+      }
     },
-    computeMeanDistance (descriptors, query) {
+    computeMeanDistance (descriptors, queryDescriptor) {
+      const {faceapi} = constants
       return faceapi.round(
         descriptors
-          .map(d => faceapi.euclideanDistance(d, query))
+          .map(d => faceapi.euclideanDistance(d, queryDescriptor))
           .reduce((d1, d2) => d1 + d2, 0) / (descriptors.length || 1)
         )
+    },
+    getBestMatch (queryDescriptor) {
+      /*
+        Args:
+          descriptorsByClass: array of objs with className and face
+                              embeddings
+             queryDescriptor: face embeddings of incoming detection
+        Returns:
+          Object {className, distance}
+      */
+      const embs = constants.db.getEmbeddings()
+
+      if (embs.length === 0) {
+        return
+      }
+
+      return embs
+        .map(
+          ({descriptors, className}) => ({
+            distance: this.computeMeanDistance(descriptors, queryDescriptor),
+            className
+          })
+        )
+        .reduce((best, curr) => best.distance < curr.distance ? best : curr)
+    },
+    classifyFace: async function (loop = false) {
+      const {db, videoEl, detectorCnv, detectorCtx,
+             maxFaceDist, unknownPrefix, minConfidence} = constants
+
+      const faceDescriptions = await this.forwardPass()
+
+      if (faceDescriptions) {
+        faceDescriptions.forEach(({detection, landmarks, descriptor}) => {
+          const {x, y, height: boxHeight, width: boxWidth} = detection.getBox()
+          const bestMatch = this.getBestMatch(descriptor)
+          console.log(bestMatch)
+          let className, color
+
+          if (detection.score < minConfidence) {
+            return
+          }
+
+          if (bestMatch && bestMatch.distance < maxFaceDist) {
+            className = `${bestMatch.className} (${bestMatch.distance})`
+            if (bestMatch.className.startsWith(unknownPrefix)) {
+              color = 'red'
+            } else {
+              color = 'green'
+            }
+          } else {
+            console.log('Tagged as unknown because dist was', bestMatch.distance, 'to best match', bestMatch.className)
+            // If class is unknown, assign it a number and
+            // save the embeddings to the database
+            className = unknownPrefix + db.getAutoIncrement()
+            color = 'red'
+            detectorCtx.drawImage(videoEl, x, y, boxHeight, boxWidth,
+                                  0, 0, detectorCnv.width, detectorCnv.height)
+
+            db.addClass(className, [descriptor], detectorCnv.toDataURL())
+          }
+
+          this.drawDetection(detection, landmarks, color, className)
+        })
+      }
+
+      if (loop) {
+        this.classifyFace(true)
+      } else {
+        videoEl.pause()
+      }
+    },
+    updateTimeStats (timeInMs) {
+      this.forwardTimes = [timeInMs].concat(this.forwardTimes).slice(0, 30)
+      // const avgTimeInMs = this.forwardTimes.reduce((total, t) => total + t) / forwardTimes.length
+      // $('#time').text(`${Math.round(avgTimeInMs)} ms`)
+      // $('#fps').text(`${faceapi.round(1000 / avgTimeInMs)}`)
     }
   }
 })
 
 Vue.component('training-app', {
-  props: ['setTrainingFlag', 'isTraining', 'drawDetection', 'forwardPass',
-          'computeMeanDistance'],
+  props: ['mode', 'setMode', 'drawDetection', 'forwardPass', 'computeMeanDistance'],
   data () {
     // It's fine for this component to have this much state because it's just temporary.
     // This state is cleared after it is sent to db after successful training.
@@ -92,39 +254,43 @@ Vue.component('training-app', {
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Method_definitions#Async_methods
     train: async function () {
       // Trains a class
-      this.setTrainingFlag(true)
+      const {minConfidence, maxFaceDist, detectorCnv, canvas,
+             canvasCtx, db, modes} = constants
+      this.setMode(modes.TRAINING)
 
       const faceDescriptions = await this.forwardPass()
 
-      faceDescriptions.forEach(({detection, landmarks, descriptor}) => {
-        if (detection.score < minConfidence) {
-          return
-        }
-
-        if (this.embeddings.length > 1) {
-          const meanDistance = this.computeMeanDistance(this.embeddings, descriptor)
-          // Don't save images too close to what we already have
-          if (meanDistance < maxFaceDist) {
-            console.log('REJECT. Image distance:', meanDistance)
+      if (faceDescriptions) {
+        faceDescriptions.forEach(({detection, landmarks, descriptor}) => {
+          if (detection.score < minConfidence) {
             return
           }
-          console.log('ACCEPT. Image distance:', meanDistance)
-        }
 
-        this.drawDetection(detection, landmarks)
+          if (this.embeddings.length > 1) {
+            const meanDistance = this.computeMeanDistance(this.embeddings, descriptor)
+            // Don't save images too close to what we already have
+            if (meanDistance < maxFaceDist) {
+              console.log('REJECT. Image distance:', meanDistance)
+              return
+            }
+            console.log('ACCEPT. Image distance:', meanDistance)
+          }
 
-        this.embeddings.push(descriptor)
-        // We save the first image taken as the display picture
-        if (this.embeddings.length === 1) {
-          // TODO this is not functional
-          this.classImage = detectorCnv.toDataURL()
-        }
-      })
+          this.drawDetection(detection, landmarks)
+
+          this.embeddings.push(descriptor)
+          // We save the first image taken as the display picture
+          if (this.embeddings.length === 1) {
+            // TODO this is not functional
+            this.classImage = detectorCnv.toDataURL()
+          }
+        })
+      }
 
       // If we have all the data we need, save to DB
       if (this.embeddings.length >= this.numTrainImages) {
-        myDB.addClass(this.trainClassName, this.embeddings, this.classImage)
-        this.setTrainingFlag(false)
+        db.addClass(this.trainClassName, this.embeddings, this.classImage)
+        this.setMode(modes.IDLE)
         // reset state
         Object.assign(this.$data, this.$options.data())
         canvasCtx.clearRect(0, 0, canvas.width, canvas.height)
@@ -137,7 +303,13 @@ Vue.component('training-app', {
     <div
       class="train-tab">
       <h3>Train the system on a face</h3>
-      <div v-if="!isTraining">
+      <div v-if="mode === 'TRAINING'">
+        <p>
+          Training class {{trainClassName}}... {{embeddings.length}}/{{numTrainImages}}.
+          Move your head around and doing other actions slowly (try blinking your eyes).
+        </p>
+      </div>
+      <div v-else>
         <p>How many pictures will the system take for training?</p>
         <div class="train-slider">
           <input
@@ -157,9 +329,6 @@ Vue.component('training-app', {
           <button @click="train">Train class</button>
         </div>
       </div>
-      <div v-else>
-        <p>Training class {{trainClassName}}... {{embeddings.length}}/{{numTrainImages}}. Move your head around!</p>
-      </div>
     </div>
   `
 })
@@ -175,7 +344,7 @@ Vue.component('face-class', {
   watch: {
     isEditing (val) {
       if (val) {
-        this.$nextTick(() => {this.$refs.inputRef.select()})
+        this.$nextTick(() => { this.$refs.inputRef.select() })
       }
     }
   },
@@ -185,11 +354,11 @@ Vue.component('face-class', {
     },
     deleteClass () {
       if (confirm(`Are you sure you want to delete class ${this.faceName}?`)) {
-        myDB.deleteClass(this.faceName)
+        constants.db.deleteClass(this.faceName)
       }
     },
     updateClass () {
-      myDB.updateClass(this.embs.className, this.faceName)
+      constants.db.updateClass(this.embs.className, this.faceName)
       this.toggleEdit()
     },
     cancel () {
